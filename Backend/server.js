09411229cb8f5509cb33de4be0e1a3cc05573a07
Jahnvi Pathak <jpathak@cmhc-schl.gc.ca>
2026-04-14 12:36:28 +0530

    Initial commit - Netflix Clone

---
+++ Backend/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";  // Import the CORS package
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
+const app = express();
+
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+
+const allowedOrigins = [
+  'http://127.0.0.1:5173/',
+  'https://stellar-griffin-33ce6a.netlify.app',
+  'https://66d47d7b82b0db0ec6aca5b5--stellar-griffin-33ce6a.netlify.app'
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
+app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/search",  searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
+  connectDB();
+});
---
+++ README.md
@@ -0,0 +1,39 @@
+# Netflix Clone
+
+This is a Netflix clone project built using the MERN stack (MongoDB, Express, React, Node.js). The project replicates the core features of Netflix, including user authentication, browsing movies and TV shows, and video streaming.
+
+## Features
+
+- **User Authentication**: Sign up, login, and logout functionalities with JWT-based authentication.
+- **Responsive UI**: A user-friendly interface that adapts to various screen sizes.
+- **Movie & TV Show Browsing**: Browse and search for movies and TV shows.
+- **Video Streaming**: Stream video content with a built-in video player.
+
+## Technologies Used
+
+- **Frontend**: React, Tailwind CSS
+- **Backend**: Node.js, Express
+- **Database**: MongoDB
+- **Authentication**: JWT (JSON Web Tokens)
+- **Styling**: Tailwind CSS
+- **Version Control**: Git & GitHub
+
+## Setup & Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/ekansh0903/Netflix-Clone.git
+   cd Netflix-Clone
+
+2. For run backend use 
+
+   ```bash
+   npm run start
+
+3. For run frontend use 
+  
+  ```bash
+   npm run dev
+
+
